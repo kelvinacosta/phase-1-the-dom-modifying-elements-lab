38c52c821cc3cabe778c09f7b990bb012d293f24
Kelvin Acosta <acosta.kelvin02@gmail.com>
2023-04-09 20:38:25 -0400

    Complete lab Dom

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Write your code here!
+
+//Remove the tag main
+const removeMain = document.querySelector('main')
+removeMain.remove()
+
+const newHeader = document.createElement('h1')
+//Create an id to the h1
+newHeader.id = 'victory'
+//Create or add text to the h1
+newHeader.textContent = 'Kelvin Acosta is the champion'
+
+//console.log(newHeader)
+
+//Appending newHeader to the body
+const body = document.querySelector('body')
+body.appendChild(newHeader)
+
